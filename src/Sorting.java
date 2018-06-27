52a20344122b3ea0da20f350e62f8d034a3a76d2
rajeshnayak789 <rajeshnayak789@gmail.com>
2018-06-27 14:50:10 +0530

    Commited and Pushed

--- src/Sorting.java
+++ src/Sorting.java
@@ -1,8 +1,5 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+
 
 public class Sorting {
     public static void main(String[] args) {
@@ -13,27 +10,23 @@ public class Sorting {
             a[a_i] = in.nextInt();
         }
         // Write Your Code Here
-
-
         int endP = a.length - 1;
         int numberOfSwaps = 0;
         while (endP > 0) {
-            int swapPosition=0;
+            int swapPosition = 0;
             for (int i = 0; i < endP; i++) {
                 if (a[i] > a[i + 1]) {
                     int tmp = a[i];
                     a[i] = a[i + 1];
                     a[i + 1] = tmp;
                     numberOfSwaps++;
-                    swapPosition=i;
+                    swapPosition = i;
                 }
-
-
             }
-            endP=swapPosition;
+            endP = swapPosition;
         }
-        System.out.println("Array is sorted in " +numberOfSwaps+" swaps.\n"
-                +"First Element: "+ a[0]+"\n"
-                +"Last Element: "+ a[n-1]+"\n");
+        System.out.println("Array is sorted in " + numberOfSwaps + " swaps.\n"
+                + "First Element: " + a[0] + "\n"
+                + "Last Element: " + a[n - 1] + "\n");
     }
 }
